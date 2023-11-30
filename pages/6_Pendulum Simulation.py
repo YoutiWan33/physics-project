1c29b24677479cfc99bdcfb36ec3f88342413890
YoutiWan33 <151761620+YoutiWan33@users.noreply.github.com>
2023-11-29 21:39:55 -0500

    Add files via upload

---
+++ pages/6_Pendulum Simulation.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import pygame
+import sys
+import math
+
+st.set_page_config(page_title="Pendulum Animation", page_icon='tada:', layout="wide")
+st.header('Pendulum Animation')
+st.subheader('Coming soon !')
