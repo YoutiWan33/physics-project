ebaae574a7adca3f718be5b26d9c5c82659fd6ee
YoutiWan33 <151761620+YoutiWan33@users.noreply.github.com>
2023-11-30 20:18:21 -0500

    Update 5_Motion_On_A_Ramp_Animation.py

--- pages/5_Motion_On_A_Ramp_Animation.py
+++ pages/5_Motion_On_A_Ramp_Animation.py
@@ -214,7 +214,7 @@ with col2:
             if i == 0:
                 time.sleep(2)  # wait a little bit longer when loading the initial position image frame
             else:
-                time.sleep(.25)  # time between each frame of image is 0.05 second
+                time.sleep(.2)  # time between each frame of image is 0.05 second
 
         # move down animation
         if np.sin(ramp_angle_radian) > np.cos(ramp_angle_radian) * Static_Friction_Coefficient:
@@ -259,7 +259,7 @@ with col2:
                 image = Image.open("motion_on_a_ramp.jpeg")
                 # load the image every 0.05 second to make a animation
                 objectmove.image(image)
-                time.sleep(.25)  # time between each frame of image is 0.05 second
+                time.sleep(.2)  # time between each frame of image is 0.05 second
 
 # object move down calculation
 with col3:
